ed3a6a199c5d332f624c71ffd271e1d253b40907
Sleeyax <yourd3veloper@gmail.com>
2019-10-14 18:29:49 +0200

    Add poll creator

---
+++ strawpoll/Migrations/20191014144520_PollModels_Poll_Creator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace strawpoll.Migrations
+{
+    public partial class PollModels_Poll_Creator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "MemberID",
+                table: "Polls",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Polls_MemberID",
+                table: "Polls",
+                column: "MemberID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Polls_Members_MemberID",
+                table: "Polls",
+                column: "MemberID",
+                principalTable: "Members",
+                principalColumn: "MemberID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Polls_Members_MemberID",
+                table: "Polls");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Polls_MemberID",
+                table: "Polls");
+
+            migrationBuilder.DropColumn(
+                name: "MemberID",
+                table: "Polls");
+        }
+    }
+}
