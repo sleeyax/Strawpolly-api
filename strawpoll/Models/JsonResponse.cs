7d8c58ecb6525f92264878dfb06eea419bd0151f
Sleeyax <yourd3veloper@gmail.com>
2019-10-15 20:58:16 +0200

    Add model to handle json responses

---
+++ strawpoll/Models/JsonResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace strawpoll.Models
+{
+    public class Status
+    {
+        public const string SUCCESS = "success";
+        public const string ERROR = "error";
+    }
+
+    public class JsonResponse
+    {
+        public string Message { get; set; }
+        public string Status { get; set; }
+        public object Data { get; set; }
+
+        public JsonResponse(string status, string message, object data = null)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+    }
+}
