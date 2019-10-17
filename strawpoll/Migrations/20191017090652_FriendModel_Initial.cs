bdfe2a422443f3e96f970ec8cce9e36f585f7324
Sleeyax <yourd3veloper@gmail.com>
2019-10-17 13:47:47 +0200

    Add friends model migration

---
+++ strawpoll/Migrations/20191017090652_FriendModel_Initial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace strawpoll.Migrations
+{
+    public partial class FriendModel_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Friends",
+                columns: table => new
+                {
+                    FriendID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FriendStatus = table.Column<int>(nullable: false),
+                    MemberID = table.Column<long>(nullable: false),
+                    MemberFriendID = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friends", x => x.FriendID);
+                    table.ForeignKey(
+                        name: "FK_Friends_Members_MemberFriendID",
+                        column: x => x.MemberFriendID,
+                        principalTable: "Members",
+                        principalColumn: "MemberID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Friends_Members_MemberID",
+                        column: x => x.MemberID,
+                        principalTable: "Members",
+                        principalColumn: "MemberID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_MemberFriendID",
+                table: "Friends",
+                column: "MemberFriendID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_MemberID",
+                table: "Friends",
+                column: "MemberID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friends");
+        }
+    }
+}
