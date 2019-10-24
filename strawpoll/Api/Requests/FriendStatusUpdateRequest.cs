e5382d456935ad5372fba48d4502df87e8e97e18
Sleeyax <yourd3veloper@gmail.com>
2019-10-24 19:23:50 +0200

    Add requests and responses for Friends

---
+++ strawpoll/Api/Requests/FriendStatusUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using strawpoll.Models;
+
+namespace strawpoll.Api.Requests
+{
+    public class FriendStatusUpdateRequest
+    {
+        public long FriendID { get; set; }
+        public FriendStatus FriendStatus { get; set; }
+    }
+}
---
+++ strawpoll/Api/Responses/FriendResponse.cs
@@ -0,0 +1,11 @@
+﻿using strawpoll.Models;
+
+namespace strawpoll.Api.Responses
+{
+    public class FriendResponse
+    {
+        public long FriendID { get; set; }
+        public FriendStatus FriendStatus { get; set; }
+        public Member Friend { get; set; }
+    }
+}
