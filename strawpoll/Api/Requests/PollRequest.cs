9e697f9c78816a5ced2a1c9f7341c42bc9ea5976
Sleeyax <yourd3veloper@gmail.com>
2019-10-27 20:49:44 +0100

    Simplify code

--- strawpoll/Api/Requests/PollRequest.cs
+++ strawpoll/Api/Requests/PollRequest.cs
@@ -1,19 +1,12 @@
 ﻿using System.Collections.Generic;
-using strawpoll.Models;
 
 namespace strawpoll.Api.Requests
 {
-    public class AnswerRequest
-    {
-        public string Answer { get; set; }
-    }
-
     public class PollRequest
     {
         public long PollID { get; set; }
         public string Name { get; set; }
-        // TODO: just use a list of strings here
-        public List<AnswerRequest> Answers { get; set; }
+        public List<string> Answers { get; set; }
         // IDs of members that can participate in this poll
         public int[] participantIds { get; set; }
     }
