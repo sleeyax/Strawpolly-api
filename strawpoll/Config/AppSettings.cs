d52f8282fb406724b297c1a92b5f9adaa7d4481d
Sleeyax <yourd3veloper@gmail.com>
2019-11-14 17:40:17 +0100

    Add email settings

--- strawpoll/Config/AppSettings.cs
+++ strawpoll/Config/AppSettings.cs
@@ -3,5 +3,7 @@
     public class AppSettings
     {
         public string JwtSecret { get; set; }
+        public EmailSettings EmailSettings { get; set; }
+        public string FrontEndUrl { get; set; }
     }
 }
---
+++ strawpoll/Config/EmailSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace strawpoll.Config
+{
+    public class EmailSettings
+    {
+        public EmailSetting MailTrap { get; set; }
+        public EmailSetting SendGrid { get; set; }
+    }
+
+    public class EmailSetting
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
